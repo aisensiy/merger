15c9764a64a2cbb55df123fd2f944975bd8aae13
aisensiy <aisensiy@gmail.com>
2014-11-03 22:57:32 +0800

    add place holder for buyer and index page

--- app/controllers/mockup_controller.rb
+++ app/controllers/mockup_controller.rb
@@ -1,13 +1,17 @@
 class MockupController < ApplicationController
   def index
+    @industries = Industry.all
   end
 
   def buyer
+    @buyers = Buyer.includes(:industry).limit(50)
   end
 
   def buy
+    @targets = Target.includes(:industry).limit(50)
   end
 
   def target
+    @targets = Target.includes(:industry).limit(50)
   end
 end
