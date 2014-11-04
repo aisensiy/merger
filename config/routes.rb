108ba8f5bc52460c4eb3a325b3967e7e7d3be925
aisensiy <aisensiy@gmail.com>
2014-11-04 22:10:54 +0800

    add new mockup flow action

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get 'mockup/target'
 
+  get 'mockup/similar_target'
+
+  get 'mockup/similar_buyer'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
