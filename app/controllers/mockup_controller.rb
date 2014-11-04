5bfb10f7c8ef01010a976e3b187cc071ddfc7c32
aisensiy <aisensiy@gmail.com>
2014-11-04 20:04:19 +0800

    make mockup business logic

--- app/controllers/mockup_controller.rb
+++ app/controllers/mockup_controller.rb
@@ -4,14 +4,23 @@ class MockupController < ApplicationController
   end
 
   def buyer
-    @buyers = Buyer.includes(:industry).limit(50)
+    @industry = Industry.find(params[:industry_id])
+    @buyers = Buyer.where(industry_id: params[:industry_id]).includes(:industry, :deals).limit(50)
   end
 
   def buy
-    @deals = Deal.includes(target: :industry, buyer: :industry)
+    @buyer_ids = params[:buyer_ids]
+    @deals = Deal.where(buyer_id: @buyer_ids).includes(target: :industry, buyer: :industry)
   end
 
   def target
-    @targets = Target.includes(:industry).limit(50)
+    @target_ids = params[:target_ids]
+    @targets = Target.where(id: @target_ids).includes(:industry)
+    avg_total_assets = @targets.map(&:total_assets).reduce(&:+)
+    @candidate_targets = Target.where('total_assets > ? and total_assets < ? and is_sold = ? and industry_id = ?',
+                                       avg_total_assets * 0.8,
+                                       avg_total_assets * 1.2,
+                                       false,
+                                       @targets[0].industry_id)
   end
 end
