9de005a6eb696e7808025b5ab5676dbcc4b2e558
aisensiy <aisensiy@gmail.com>
2014-12-11 17:44:49 +0800

    finish busi logic for get targets

--- app/models/target_v2.rb
+++ app/models/target_v2.rb
@@ -1,2 +1,31 @@
 class TargetV2 < ActiveRecord::Base
+  extend SimilarCalculatable
+  
+  def self.similar(candidates, similar_targets, attrs, k=10)
+    vectors = similar_targets.map do |target|
+      target.attributes.select { |k, v| attrs.include? k }.values.map do |v|
+        v.nil? ? 0 : v
+      end
+    end
+
+    center = attrs.size.times.map do |i|
+      sum = 1.0 * vectors.size.times.map { |j| vectors[j][i] }.reduce(&:+)
+      sum / similar_targets.size
+    end
+
+    candidate_matrix = candidates.map do |candidate|
+      candidate.attributes.select { |k, v| attrs.include? k }.values.map do |v|
+        v.nil? ? 0 : v
+      end
+    end
+
+    matrix = [center] + candidate_matrix
+    normalized_matrix = transpose(transpose(matrix).map { |v| normalize(v) })
+
+    distances = (1..candidates.size).map do |i|
+      [cal_distance(normalized_matrix[0], normalized_matrix[i]), candidates[i - 1]]
+    end.sort { |a, b| a[0] <=> b[0] }
+
+    distances.map { |d| [d[1], d[0]] }[0...k]
+  end
 end
